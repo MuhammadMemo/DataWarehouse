d3719b7463ac651b72270ec7748511251c9dfdc5
ism01 <ism01@RPT-SRV>
2023-04-10 19:32:21 +0200

    Add project files.

---
+++ DimCompany.sql
@@ -0,0 +1,9 @@
+﻿
+-- DimCompany (Type:Static ,Loading:Initial Only)
+-- Three Company (Mfco-MT-ME-DAT:test)
+
+CREATE TABLE [dbo].[DimCompany]
+(
+	[Id] tinyint NOT NULL PRIMARY KEY,
+	CompanyName   nvarchar(10) NOT NULL 
+)
