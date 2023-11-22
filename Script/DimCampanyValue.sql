356355ba7a864f34790b403f30f9acc51a46602f
MuhammadMahmoud <programmer.muhammad@gmail.com>
2023-11-22 10:33:21 +0200

    rrf

---
+++ Script/DimCampanyValue.sql
@@ -0,0 +1,9 @@
+﻿use DataWarehouse
+go
+
+
+
+INSERT INTO [dbo].[DimCompany] ([CompanyAlternativeKey],[CompanyName]) VALUES ('1','Mfco')
+   INSERT INTO [dbo].[DimCompany] ([CompanyAlternativeKey],[CompanyName]) VALUES ('2','MT')        
+
+GO
