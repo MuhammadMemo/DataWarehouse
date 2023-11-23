ea2ca2f1f6a175b6a1cec0870fbc6a5ea9da2a2d
MuhammadMahmoud <programmer.muhammad@gmail.com>
2023-11-23 19:26:03 +0200

    dd

--- Tables/DimCar.sql
+++ Tables/DimCar.sql
@@ -3,9 +3,10 @@
 	CompanyKey tinyint NOT NULL,
 	CarKey smallint identity (1,1),
 	CarAlternativeKey nvarchar(30) NOT NULL,
-	CarName nvarchar(30) NOT NULL,
-	Description nvarchar(30) NOT NULL,
-	
+	CarNumber nvarchar(20) NOT NULL,
+	CarName nvarchar(150) NOT NULL,
+	CarType tinyint NOT NULL,
+	Description nvarchar(150) NOT NULL,
 	UpdatLasteTime datetime default getdate(),
 
 CONSTRAINT [PK_DimCar_1] PRIMARY KEY CLUSTERED 
