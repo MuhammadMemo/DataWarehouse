def73f3fbaf525ff304655bd79ac7afde2cddb76
MuhammadMahmoud <programmer.muhammad@gmail.com>
2023-11-24 04:39:28 +0200

    ddd

--- Tables/DimInventStatusReceipt.sql
+++ Tables/DimInventStatusReceipt.sql
@@ -1,4 +1,4 @@
-﻿CREATE TABLE [dbo].[DimStatusReceipt]
+﻿CREATE TABLE [dbo].[DimInventStatusReceipt]
 (
 	StatusReceiptKey tinyint NOT NULL identity (1,1) PRIMARY KEY,
 	StatusReceiptName nvarchar(50) Not null
--- Tables/DimRegion.sql
+++ Tables/DimRegion.sql
@@ -2,9 +2,9 @@
 (
 CompanyKey tinyint NOT NULL,
 RegionKey int identity (1,1) ,
-CountryAlternativeKey nvarchar(20) NOT NULL,
-State nvarchar(50) NOT NULL,
-City nvarchar(50) NOT NULL,
+CountryAlternativeKey nvarchar(50) NOT NULL,
+State nvarchar(50)  ,
+City nvarchar(100)  ,
 UpdatLasteTime datetime default GetDate(),
 CONSTRAINT [PK_DimRegion] PRIMARY KEY CLUSTERED 
 (
