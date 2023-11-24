7c67c90305af1741bad40a33a672e75dda69f316
MuhammadMahmoud <programmer.muhammad@gmail.com>
2023-11-24 04:53:34 +0200

    xx

--- Tables/DimUnit.sql
+++ Tables/DimUnit.sql
@@ -2,8 +2,8 @@
 (
 	CompanyKey tinyint NOT NULL,
 	UnitKey  int  IDENTITY (1,1) ,
-	UnitAlternativeKey  int not null ,
-	UnitName nvarchar(50) not null ,
+	UnitAlternativeKey  nvarchar(50)  not null ,
+	UnitName nvarchar(150) not null ,
 	UpdatLasteTime datetime Default Getdate(),
 	
 CONSTRAINT [PK_DimUnit_1] PRIMARY KEY CLUSTERED 
