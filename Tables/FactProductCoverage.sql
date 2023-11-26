bc4c99623fec2ebcdc2b43b3d1a9c7aa789e118d
MuhammadMahmoud <programmer.muhammad@gmail.com>
2023-11-26 14:21:24 +0200

    eee

--- Tables/FactProductCoverage.sql
+++ Tables/FactProductCoverage.sql
@@ -1,9 +1,10 @@
-﻿CREATE TABLE [dbo].[FactPro]
+﻿CREATE TABLE [dbo].[FactProductCoverage]
 (
 	CompanyKey Tinyint not null,
 	[Id] INT NOT NULL PRIMARY KEY,
 	ProductMin DECIMAL Default 0.0,
 	ProductMax DECIMAL Default 0.0,
+	LeadTimePurchase int  Default 0.0,
 	DimensionKey  int  not null,
 	ProductKey int  not null
 	
