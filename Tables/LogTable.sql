bc51a24a75abd88012ef076adba127f0537256c9
MuhammadMahmoud <programmer.muhammad@gmail.com>
2023-11-29 13:35:40 +0200

    Run Success All Dim

--- Tables/LogTable.sql
+++ Tables/LogTable.sql
@@ -1,15 +1,16 @@
 ﻿CREATE TABLE [dbo].[LogTable](
 	[Id] [int] IDENTITY(1,1) NOT NULL PRIMARY KEY,
+	[TableNo] tinyint not null ,
+	[Batch] [int]  NULL,
 	[Pakage] [nvarchar](50) NULL,
 	[MachineName] [nvarchar](50) NULL,
 	[TableName] [nvarchar](50) NULL,
 	[IsInserted] [int] NULL,
 	[IsUpdated] [int] NULL,
 	[IsDeleted] [int] NULL,
-	[ModifiedDate] [datetime] NULL,
-	[StartPakage] [datetime] NULL,
 	[Note] [nvarchar](max) NULL,
-	TableNo tinyint not null , 
-	Status nvarchar (30) not null,
+	[Status] nvarchar (30) not null,
+	[StartPakage] [datetime] NULL,
+	[ModifiedDate] [datetime] NULL,
 	[ScondsDiff]  AS (datediff(minute,[StartPakage],[ModifiedDate]))
 	)
