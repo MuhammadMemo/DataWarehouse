d9a58f08f396b1eb3095909bdb48f14ae1209ac8
MuhammadMahmoud <programmer.muhammad@gmail.com>
2023-11-29 14:59:49 +0200

    rrr

---
+++ Tables/DimEmpDeductionAllowance.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[DimEmpDeductionAllowance]
+(
+	 CompanyKey tinyint not null,
+ 	 CodeKey int identity (1,1),
+     CodeAlternativeKey int not null,
+     DeductionAllowanceName nvarchar(200),
+     DeductionAllowanceType tinyint not null ,
+     PayType nvarchar(20),
+     UpdatLasteTime datetime default getdate(),
+
+CONSTRAINT [PK_DimDecAllow_1] PRIMARY KEY CLUSTERED 
+(
+	CodeKey ASC,
+    CompanyKey ASC)
+)ON [PRIMARY]
