9fabd171303f969859d39d61be69315f073b8728
MuhammadMahmoud <programmer.muhammad@gmail.com>
2023-11-30 15:44:46 +0200

    dd

--- Tables/DimEmpHoliday.sql
+++ Tables/DimEmpHoliday.sql
@@ -1,4 +1,15 @@
 ﻿CREATE TABLE [dbo].[DimEmpHoliday]
 (
-	[Id] INT NOT NULL PRIMARY KEY
-)
+	CompanyKey tinyint NOT NULL,
+	HolidayKey int identity (1,1),
+	HolidayName nvarchar(100) not null,
+    YEARS int not null,
+	HolidayDate date not null,
+	UpdatLasteTime datetime default getdate(),
+
+CONSTRAINT [PK_DimHoliday_1] PRIMARY KEY CLUSTERED 
+(
+	HolidayKey ASC,
+    CompanyKey ASC)
+)ON [PRIMARY]
+
