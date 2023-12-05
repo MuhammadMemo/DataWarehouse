1991e093fae5312026c415682193d0607bbc80fd
MuhammadMahmoud <programmer.muhammad@gmail.com>
2023-12-05 13:14:54 +0200

    eeee

--- Tables/DimEmployee.sql
+++ Tables/DimEmployee.sql
@@ -6,16 +6,17 @@
 	EmployeeName nvarchar(420) NOT NULL,
 	Job nvarchar(100) NOT NULL,
 	DepartmenName nvarchar(100) NOT NULL,
-	DivisionName  nvarchar(100)  NULL,
-	Gender nvarchar(15) ,
-	ContractType  nvarchar(40),
-    ContractDate Date,
-    ContractEndDate Date,
-	BirthDate Date,
-	HireDate Date,
-	InsuranceDate Date,
-	WorkeStatus nvarchar(30)  NULL,
-	UpdatLasteTime datetime default getdate(),
+	DivisionName  nvarchar(100) default 'UnKown' ,
+	Gender nvarchar(15)  default 'UnKown'  ,
+	ContractType  nvarchar(40) default 'UnKown' ,
+    ContractDate Date default '1900-01-01' ,
+    ContractEndDate Date default '1900-01-01',
+	BirthDate Date default '1900-01-01',
+	HireDate Date default '1900-01-01',
+	InsuranceDate Date default '1900-01-01',
+	WorkeStatus nvarchar(30)  default 'UnKown' ,
+	UpdatLasteTime datetime default getdate() NOT NULL,
+
 
 CONSTRAINT [PK_DimEmployee_1] PRIMARY KEY CLUSTERED 
 (
