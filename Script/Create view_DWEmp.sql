239460ab423fe72b025504f0823e8a7dd16adb52
MuhammadMahmoud <programmer.muhammad@gmail.com>
2023-12-05 11:27:12 +0200

    fffss

---
+++ Script/Create view_DWEmp.sql
@@ -0,0 +1,4 @@
+﻿
+alter view [dbo].[_DWEmp]  as 
+select EMPLID,d.NAME,e.DATAAREAID as Companyname from EMPLTABLE	   e
+left join 	  DirPartyTable	   d	 on e.PARTYID=d.PARTYID		  and e.DATAAREAID=d.DATAAREAID
