3c12df7271dd48109605de02426c7ddfd1ac873e
MuhammadMahmoud <programmer.muhammad@gmail.com>
2023-12-12 14:58:45 +0200

    dfdf

--- Script/Create_DWTargetLevels.sql
+++ Script/Create_DWTargetLevels.sql
@@ -8,9 +8,9 @@ select * from SalesTarget
 
 select * from SalesCommission
 
---alter View _DWFactSalesCommission as 
+alter View _DWFactSalesCommission as 
 select t.DATAAREAID as CompanyName, t.RECID as TargetLevelAlternativeKeySource , t.DATE as TargetDateKeySource ,t.AMOUNTFROM as AmountFromSource ,t.AMOUNTto as AmountToSource,t.TYPE as TypeSource,t.PRECENT as PrecentSource,t.LEVEL_ as LevelSource,
- CAST(REPLACE(CONVERT(date, t.DATE), '-', '') AS int) AS TargetDateAlternativeKeySource,
+ CAST(REPLACE(CONVERT(date, t.DATE), '-', '') AS int) AS TargetDateAlternativeKeySource,t.COMMISSSALESGROUP as BrancheNameSource,
 isnull(c.USER_,0) as EmpKeySource,isnull(c.AMOUNT,0) as AmountSource,isnull(c.COUNT,0)  as CountSoSource,isnull(c.Total,0)  as TotalSalesSource 
 from SalesTarget t left join SalesCommission c on t.RECID=c.ID_LEVEL and t.DATAAREAID=c.DATAAREAID
 where c.COUNT is not null
--- Tables/FactSalesCommission.sql
+++ Tables/FactSalesCommission.sql
@@ -8,6 +8,7 @@
 	  TargetLevelkey tinyint NOT NULL,
 	  EmpTypeKey tinyint NOT NULL, 
 	  EmployeeKey int NOT NULL,
+	  BrancheKey tinyint NOT NULL, 
 	  TargetDateAlternativeKey int NOT NULL,
 	  
 	  TargetDateKey  date NOT NULL, 
@@ -25,6 +26,7 @@
     CONSTRAINT [PK_FactSalesCommission] PRIMARY KEY (CompanyKey,SalesCommissionKey) ,
 	CONSTRAINT [PF_TargetLevel] FOREIGN KEY  (TargetLevelkey,CompanyKey) REFERENCES DimTargetLevel(TargetLevelkey,CompanyKey),
 	CONSTRAINT [PF_EmployeeTarget] FOREIGN KEY  (EmployeeKey,CompanyKey) REFERENCES DimEmployee(EmployeeKey,CompanyKey),
+	CONSTRAINT [PF_BrancheTarget] FOREIGN KEY  (BrancheKey,CompanyKey) REFERENCES DimBranche(BrancheKey,CompanyKey),
     CONSTRAINT [PF_CompanyTarget] FOREIGN KEY (CompanyKey) REFERENCES DimCompany(CompanyKey),
     CONSTRAINT [PF_DateTarget] FOREIGN KEY (TargetDateAlternativeKey) REFERENCES DimDate(DateKey),
 
