99a0bfc045ebb077cda65122f4b0a3e3f59f0711
MuhammadMahmoud <programmer.muhammad@gmail.com>
2023-12-12 19:58:58 +0200

    rer

--- Script/Create_DWTargetLevels.sql
+++ Script/Create_DWTargetLevels.sql
@@ -8,7 +8,7 @@ select * from SalesTarget
 
 select * from SalesCommission
 
-alter View _DWFactSalesCommission as 
+--alter View _DWFactSalesCommission as 
 select t.DATAAREAID as CompanyName, t.RECID as TargetLevelAlternativeKeySource , t.DATE as TargetDateKeySource ,t.AMOUNTFROM as AmountFromSource ,t.AMOUNTto as AmountToSource,t.TYPE as TypeSource,t.PRECENT as PrecentSource,t.LEVEL_ as LevelSource,
  CAST(REPLACE(CONVERT(date, t.DATE), '-', '') AS int) AS TargetDateAlternativeKeySource,t.COMMISSSALESGROUP as BrancheNameSource,
 isnull(c.USER_,0) as EmpKeySource,isnull(c.AMOUNT,0) as AmountSource,isnull(c.COUNT,0)  as CountSoSource,isnull(c.Total,0)  as TotalSalesSource 
