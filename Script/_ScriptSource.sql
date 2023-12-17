28a59fa818630491f6837f5edb1c5051e387cead
MuhammadMahmoud <programmer.muhammad@gmail.com>
2023-12-17 14:30:59 +0200

    fdfd

--- Script/_ScriptSource.sql
+++ Script/_ScriptSource.sql
@@ -13,6 +13,12 @@ order by 	  left(SALESID,2)
 --delete	 SALESline  where salesid like'05%' 
 	 --count(*),
 select  SALESID,  SHIPPINGDATEREQUESTED,DATAAREAID from  SALESTABLE	
-where SALESID like '00%'  and    ( SHIPPINGDATEREQUESTED >= CONVERT(datetime, '2013-01-01 00:00:00.000', 102))
+where SALESID like '00%'  and    ( SHIPPINGDATEREQUESTED = CONVERT(datetime, '2023-12-17 00:00:00.000', 102))
 and SALESGROUP <>'07'		and SALESSTATUS <> 4
 order by  SALESID, SHIPPINGDATEREQUESTED
+
+
+select  SalesOrderNumber,MODIFIEDDATETIME from  _DWFactSales
+where    ( OrderDateSource = CONVERT(datetime, '2023-12-17 00:00:00.000', 102))
+order by MODIFIEDDATETIME
+
--- Script/_Scriptdestination.sql
+++ Script/_Scriptdestination.sql
@@ -1,13 +1,18 @@
-﻿select * from 	 [dbo].[LogTable]	   where 	  [Batch]	in (select max([Batch])  from 	 [dbo].[LogTable]	) 
+﻿select * from [dbo].[LogTable] where [Batch] in(select max([Batch]) from [dbo].[LogTable]) 
 
-select SUM(SecondDiff)as SecondDiff ,sum(MinuteDiff)  as MinuteDiff from 	 [dbo].[LogTable]	   where 	  [Batch]	in (select max([Batch])  from 	 [dbo].[LogTable]	) 
+select SUM(SecondDiff)as SecondDiff ,sum(MinuteDiff)  as MinuteDiff from [dbo].[LogTable] where [Batch]	in (select max([Batch]) from [dbo].[LogTable]) 
 
 select sum(ScondsDiff) from 	 [dbo].[LogTable]	   where 	  [Batch]	in (select max([Batch])   from 	 [dbo].[LogTable]	)
 
+select * from [dbo].[LogTable] where   Tableno=20
 
 select count(*) from  FactSales	  where IsDeleted=0
 
+--delete [dbo].[LogTable] where [Batch] <> 105 and tableNo=20
 
+
+select  SalesOrderNumber ,LastupdateDate from   FactSales
+where    ( OrderDate = CONVERT(datetime, '2023-12-17 00:00:00.000', 102))
 --truncate table dimemptype
 --truncate table FactSales
 
