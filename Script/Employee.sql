e18123b2af37e5b0b983377a174a910ca673220f
MuhammadMahmoud <programmer.muhammad@gmail.com>
2024-01-18 11:19:17 +0200

    fff

--- Script/Employee.sql
+++ Script/Employee.sql
@@ -1,6 +1,6 @@
 ﻿
 alter view [dbo].[_DWEmp]  as 
-select EMPLID,d.NAME,e.DATAAREAID as Companyname from EMPLTABLE	   e
+select EMPLID,d.NAME,status,e.DATAAREAID as Companyname from EMPLTABLE	   e
 left join 	  DirPartyTable	   d	 on e.PARTYID=d.PARTYID		  and e.DATAAREAID=d.DATAAREAID
 where d.name is not null
 
